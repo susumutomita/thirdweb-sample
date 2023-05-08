2ca54a04e3d421f0d393cf07cbb4e4d44b0bce42
susumu tomita <oyster880@gmail.com>
2023-05-08 09:57:57 +0900

    add sample

---
+++ src/sample.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.0;
+
+import "@thirdweb-dev/contracts/base/ERC1155LazyMint.sol";
+
+contract sample is ERC1155LazyMint {
+    constructor(
+        string memory _name,
+        string memory _symbol,
+    ) ERC1155LazyMint(_name, _symbol, msg.sender, 0) {}
+
+    function verifyClaim(
+        address _claimer,
+        uint256 _tokenId,
+        uint256 _quantity,
+    ) public view override {
+        require (_tokenId == 0, "Only sushi are claimed!");
+        require (_quantity == 1, "Only one sushi can be claimed!");
+    }
+
+    function evolve(){
+    _burn(msg.sender, 0, 2)
+    _mint(msg.sender, 1, 1,"")
+    }
+}
