02fc13c0df78ef20bbecd0178b51d4a4fd2fd44d
susumu tomita <oyster880@gmail.com>
2023-05-08 10:59:07 +0900

    update

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+# 基本イメージとして、Node.js がインストールされたイメージを選択
+FROM node:16
+
+SHELL ["/bin/bash", "-c"]
+ENV PATH="~/.foundry/bin:${PATH}"
+
+# 作業ディレクトリを設定
+WORKDIR /app
+
+# 必要なパッケージのインストールとFoundryのインストールを1つのRUNで実行
+RUN apt-get update && \
+  apt-get install -y curl bash yarn && \
+  curl -L https://foundry.paradigm.xyz | bash && \
+  source ~/.bashrc && \
+  foundryup
+
+# 依存関係をインストール
+COPY package*.json yarn.lock ./
+RUN yarn install
+
+# アプリケーションのソースコードをコピー
+COPY . .
--- src/sample.sol
+++ src/sample.sol
@@ -6,20 +6,20 @@ import "@thirdweb-dev/contracts/base/ERC1155LazyMint.sol";
 contract sample is ERC1155LazyMint {
     constructor(
         string memory _name,
-        string memory _symbol,
+        string memory _symbol
     ) ERC1155LazyMint(_name, _symbol, msg.sender, 0) {}
 
     function verifyClaim(
         address _claimer,
         uint256 _tokenId,
-        uint256 _quantity,
+        uint256 _quantity
     ) public view override {
         require (_tokenId == 0, "Only sushi are claimed!");
         require (_quantity == 1, "Only one sushi can be claimed!");
     }
 
-    function evolve(){
-    _burn(msg.sender, 0, 2)
-    _mint(msg.sender, 1, 1,"")
+    function evolve() public{
+            _burn(msg.sender, 0, 1);
+            _mint(msg.sender, 1, 1,"");
     }
 }
